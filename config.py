15be8671abcaddd10b879aadc00c134e31f2a15d
bulat <damask161092@gmail.com>
2019-08-20 16:33:53 +0500

    hghg

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot('930440634:AAEbV5dV3vzRHWlotbe4vKGC4M6zOIFvnzI')# токе
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True) # клавыатуры
 keyboard1.row('kushnarenkovo', 'ufa')
 keyboard2 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard2.row('bye')
+keyboard2.row('No thank you bye', 'Yes, I want to know the temperature in another city')
 keyboard3 = telebot.types.ReplyKeyboardMarkup(True, True)
 keyboard3.row('/start')
 
@@ -20,15 +20,18 @@ def start_message(message):
     if message.text == 'kushnarenkovo': # если кушнаренково
         observation = owm.weather_at_place('kushnarenkovo,RU') # достать погоду
         w = observation.get_weather() # достать погоду
-        bot.send_message(message.chat.id, 'Air temperature in Kushnarenkovo ' + str(w.get_temperature('celsius')["temp"]) + "°C", reply_markup=keyboard2) # поголдду градусы отправь
+        bot.send_message(message.chat.id, 'Air temperature in Kushnarenkovo ' + str(w.get_temperature('celsius')["temp"]) + "°C \n Anything else", reply_markup=keyboard2) # поголдду градусы отправь
     
     elif message.text == 'ufa':
         observation = owm.weather_at_place('ufa,RU')
         w = observation.get_weather()
-        bot.send_message(message.chat.id,'Air temperature in Ufa ' + str(w.get_temperature('celsius')["temp"]) + "°C" , reply_markup=keyboard2)
+        bot.send_message(message.chat.id,'Air temperature in Ufa ' + str(w.get_temperature('celsius')["temp"]) + "°C \n Anything else" , reply_markup=keyboard2)
 
-    elif  message.text == 'bye':
+    elif  message.text == 'No thank you bye':
         bot.send_message(message.chat.id, 'Goodbye see you tomorrow', reply_markup=keyboard3)
+
+    elif  message.text == 'Yes, I want to know the temperature in another city':
+        bot.send_message(message.chat.id, 'In which', reply_markup=keyboard2)
 
 
 bot.polling() # крутись живи вечно
