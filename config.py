c23493a5b5b384c3351e800c81a28409ad1fd3f4
bulat92 <49103720+bulat92@users.noreply.github.com>
2020-05-20 17:22:25 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import pyowm # погодоный информатор
 
 owm = pyowm.OWM('8abe2497f6167e77e46f4d329a58e800', language = "ru") # токен для информатора и выбор языка  
 bot = telebot.TeleBot('930440634:AAEbV5dV3vzRHWlotbe4vKGC4M6zOIFvnzI')# токен бота и 
-keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True) # клавыатуры
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True) # клавиатуры
 keyboard1.row('kushnarenkovo', 'ufa')
 keyboard2 = telebot.types.ReplyKeyboardMarkup(True, True)
 keyboard2.row('No thank you bye')
@@ -13,9 +13,9 @@ keyboard3.row('/start')
 
 
 
-@bot.message_handler(commands=['start']) #фильтр обраблотчик
+@bot.message_handler(commands=['start']) #фильтр обработчик
 def first_message(message): # имя функций
-    bot.send_message(message.chat.id, "Hello you wrote me /start \nWhat city temperature do you want to know?", reply_markup=keyboard1) # бот отправляет . чат иб ответ и вывыводит клаву
+    bot.send_message(message.chat.id, "Hello you wrote me /start \nWhat city temperature do you want to know?", reply_markup=keyboard1) # бот отправляет в чат ответ и вывыводит клаву
     
 
 @bot.message_handler(content_types=['text'])
@@ -24,7 +24,7 @@ def start_message(message):
     if message.text == 'kushnarenkovo': # если кушнаренково
         observation = owm.weather_at_place('kushnarenkovo,RU') # достать погоду
         w = observation.get_weather() # достать погоду
-        bot.send_message(message.chat.id, 'Air temperature in Kushnarenkovo ' + str(w.get_temperature('celsius')["temp"]) + "°C \nAnything else", reply_markup=keyboard2) # поголдду градусы отправь
+        bot.send_message(message.chat.id, 'Air temperature in Kushnarenkovo ' + str(w.get_temperature('celsius')["temp"]) + "°C \nAnything else", reply_markup=keyboard2) # градусы отправить 
     
     elif message.text == 'ufa':
         observation = owm.weather_at_place('ufa,RU')
@@ -40,4 +40,4 @@ def start_message(message):
     else:
         bot.send_message(message.chat.id, 'Hello', reply_markup=keyboard3)
 
-bot.polling() # крутись живи вечно
+bot.polling() # запуск 
